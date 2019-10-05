92c8d7166655aadaf834e3c15f4989b046faa224
G Roques <groques360@gmail.com>
2019-10-04 20:52:36 -0500

    Convert double quotes to single quotes

--- pdf2json.js
+++ pdf2json.js
@@ -7,7 +7,7 @@ const PDFParser = require('pdf2json');
 const pdfParser = new PDFParser();
 
 pdfParser.on('pdfParser_dataError', errData => console.error(errData.parserError));
-pdfParser.on("pdfParser_dataReady", pdfData => {
-    fs.writeFile("./WildEdibles.json", JSON.stringify(pdfData));
+pdfParser.on('pdfParser_dataReady', pdfData => {
+    fs.writeFile('./WildEdibles.json', JSON.stringify(pdfData));
 });
-pdfParser.loadPDF("./WildEdibles.pdf");
+pdfParser.loadPDF('./WildEdibles.pdf');
